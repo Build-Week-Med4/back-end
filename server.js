305712026e3269cb11abf3f638111295df2762eb
Steven Waldron <stevenwaldron@stevens-mbp.attlocal.net>
2020-08-30 20:11:44 -0700

    MVP 3

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const recommendationsRouter = require('./routers/recommendationsRouter')
 const cors = require('cors')
 
 server.use(cors())
-server.use(function(req, res, next) {
-    if (!req.headers.token) {
-      return res.status(403).json({ error: 'you must be logged in first!' });
-    }
-    next();
-  });
 server.use(express.json())
 server.use('/users',usersRouter)
+server.use(function(req, res, next) {
+  if (!req.headers.token) {
+    return res.status(403).json({ error: 'you must be logged in first!' });
+  }
+  next();
+});
 server.use('/recommendations',recommendationsRouter)
+
 
 
 module.exports = server
